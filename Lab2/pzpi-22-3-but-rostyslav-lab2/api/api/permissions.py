9edfa043250e1e6f74e8080a1fdef3ed2241282d
But Rostyslav <rostyslav.but@nure.ua>
2025-06-12 00:57:37 +0300

    Delete Lab2/pzpi-22-3-but-rostyslav-lab2/api directory

--- Lab2/pzpi-22-3-but-rostyslav-lab2/api/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUserOnly(BasePermission):
-    """
-    Дозволяє доступ лише адміністраторам.
-    """
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_staff
--- Lab2/pzpi-22-3-but-rostyslav-lab2/api/api/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/realtime-expenses/(?P<user_id>\d+)/$', consumers.ExpenseConsumer.as_asgi()),
-]
